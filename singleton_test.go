50d030a417d45c8c6e1b0c5b27f42497c5e08e29
purecallxu <purecallxu@tencent.com>
2020-03-19 16:27:16 +0800

    添加了单例模式demo

---
+++ singleton_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"unsafe"
+)
+
+type Singleton struct {
+}
+
+var singleInstance *Singleton
+var once sync.Once
+
+func GetSingletonObj() *Singleton {
+	once.Do(func() {
+		fmt.Println("Create Obj") // 只输出了一次，说明对象只创建了一次
+		singleInstance = new(Singleton)
+	})
+	return singleInstance
+}
+
+func TestGetSingletonObj(t *testing.T) {
+	var wg sync.WaitGroup
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go func() {
+			obj := GetSingletonObj()
+			fmt.Printf("%x\n", unsafe.Pointer(obj))
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+/*
+Create Obj
+677188
+677188
+677188
+*/
