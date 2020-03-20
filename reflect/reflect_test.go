a43b0c813ea253688bfc12a47268bc5a8f870cc5
purecallxu <purecallxu@tencent.com>
2020-03-21 02:14:28 +0800

    add reflect demo

--- reflect/reflect_test.go
+++ reflect/reflect_test.go
@@ -1,6 +1,7 @@
 package reflect_test
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
 	"testing"
@@ -72,4 +73,91 @@ func TestInvokeByName(t *testing.T) {
 	   PASS
 	*/
 
+}
+
+func TestDeepEqual(t *testing.T) {
+	a := map[int]string{1: "one", 2: "two", 3: "three"}
+	b := map[int]string{1: "one", 2: "two", 3: "three"}
+	// t.Log(a == b) // invalid operation
+	t.Log(reflect.DeepEqual(a, b))
+
+	s1 := []int{1, 2, 3}
+	s2 := []int{1, 2, 3}
+	s3 := []int{2, 3, 1}
+	t.Log("s1==s2?", reflect.DeepEqual(s1, s2))
+	t.Log("s1==s3?", reflect.DeepEqual(s1, s3))
+	/*
+		=== RUN   TestDeepEqual
+		--- PASS: TestDeepEqual (0.00s)
+		    reflect_test.go:86: true
+		    reflect_test.go:91: s1==s2? true
+		    reflect_test.go:92: s1==s3? false
+		PASS
+	*/
+}
+
+type Customer struct {
+	CookieID string
+	Name     string
+	Age      int
+}
+
+//两个结构体，都有Name和Age字段
+//写一个通用的函数，使其能够对不同结构体的相同字段进行赋值
+func fillBySettings(st interface{}, settings map[string]interface{}) error {
+	// func(v Value) Elem() Value
+	// Elem() returns the value that the interface v contains or that the pointer points to
+	// It panics if v's Kind is not Interface of Ptr
+	// Ite returns the zero Value if v is nil
+
+	if reflect.TypeOf(st).Elem().Kind() != reflect.Ptr {
+		// 传入的是指针类型，Elem() 获取指针指向的具体结构
+		if reflect.TypeOf(st).Elem().Kind() != reflect.Struct {
+			return errors.New("the first param should be a pointer to the struct type.")
+		}
+	}
+
+	if settings == nil {
+		return errors.New("settings is nil.")
+	}
+
+	var (
+		field reflect.StructField
+		ok    bool
+	)
+
+	for k, v := range settings {
+		//遍历map的key，在结构体中找相同的名字
+		if field, ok = (reflect.ValueOf(st)).Elem().Type().FieldByName(k); !ok {
+			continue
+		}
+		if field.Type == reflect.TypeOf(v) {
+			vstr := reflect.ValueOf(st)
+			vstr = vstr.Elem()
+			vstr.FieldByName(k).Set(reflect.ValueOf(v))
+		}
+	}
+	return nil
+}
+
+func TestFillNameAndAge(t *testing.T) {
+	settings := map[string]interface{}{"Name": "Mike", "Age": 20}
+	e := Employee{}
+	if err := fillBySettings(&e, settings); err != nil {
+		t.Fatal(err)
+	}
+	t.Log(e)
+
+	c := new(Customer)
+	if err := fillBySettings(c, settings); err != nil {
+		t.Fatal(err)
+	}
+	t.Log(*c)
+	/*
+		=== RUN   TestFillNameAndAge
+		--- PASS: TestFillNameAndAge (0.00s)
+		    reflect_test.go:147: { Mike 20}
+		    reflect_test.go:153: { Mike 20}
+		PASS
+	*/
 }
