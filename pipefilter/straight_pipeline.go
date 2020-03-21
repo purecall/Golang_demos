8b20ed0ba440175847829234bbe0c4a7c378f22b
purecallxu <purecallxu@tencent.com>
2020-03-21 22:55:23 +0800

    add pipefilter demo

---
+++ pipefilter/straight_pipeline.go
@@ -0,0 +1,27 @@
+package pipefilter
+
+type StraightPipeline struct {
+	Name    string
+	Filters *[]Filter
+}
+
+func NewStraightPipeline(name string, filters ...Filter) *StraightPipeline {
+	return &StraightPipeline{
+		Name:    name,
+		Filters: &filters,
+	}
+}
+
+// Process is to process the coming data by the pipeline
+func (f *StraightPipeline) Process(data Request) (Response, error) {
+	var ret interface{}
+	var err error
+	for _, filter := range *f.Filters {
+		ret, err = filter.Process(data)
+		if err != nil {
+			return ret, err
+		}
+		data = ret
+	}
+	return ret, nil
+}
