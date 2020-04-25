f61d9639917ee1b6bdac8f4f962faf0116d14f82
purecallxu <purecallxu@tencent.com>
2020-04-26 03:17:01 +0800

    delete mercury

--- mercury/session/session_mgr.go
+++
@@ -1,7 +0,0 @@
-package session
-
-type SessionMgr interface {
-	Init(addr string, options ...string) (err error)
-	CreateSession() (session Session, err error)
-	Get(sessionId string) (session Session, err error)
-}
